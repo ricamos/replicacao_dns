f5f7cf109c46d6cd0ed9715eda933ae7d92b14a1
Ricardo Coelho <ricardo.rcj@gmail.com>
2021-09-02 00:18:24 -0300

    feat: Replicação DNS com firewall DROP
    Diferente da versão anterior o firewall agora esta em FORWARD DROP e
    precisa de regras especificas para libearação de transferencia de zona
    DNS.
    
    testes:
    
    Host
    dig -t SOA exemplo.local @localhost +noall +answer
    dig -t SOA exemplo.local @192.168.42.20 +noall +answer
    dig -t SOA exemplo.local @172.16.42.20 +noall +answer
    
    NS1
    Adicionar registro "teste1" no DNS primario e alterar o serial
    vim /var/cache/bind/db.exemplo
    
    ns1
    tcpdump -i 3 ! port 22 and host 172.16.42.20 -vvvn and ip
    
    fw1
    tcpdump -i 3 ! port 22 and ip -vvvn and ip
    
    ns2
    tcpdump -i 3 ! port 22 and host 192.168.42.20 -vvvn and ip
    
    Host
    vagrant ssh ns1 -- ping 172.16.42.20 -c5
    vagrant ssh ns1 -- sudo systemctl restart bind9
    vagrant ssh ns1 -- ping 172.16.42.20 -c5
    
    dig -t A teste1.exemplo.local @192.168.42.20 +noall +answer
    dig -t A teste1.exemplo.local @172.16.42.20 +noall +answer
    
    ns2
    dig AXFR exemplo.local @192.168.42.20
    
    HEAD detached at e68b7f8
     Changes to be committed:
    	modified:   files/fw1.sh

--- files/fw1.sh
+++ files/fw1.sh
@@ -1,6 +1,31 @@
 firewall_config() {
+
+	NS1=192.168.42.20
+	NS2=172.16.42.20
+
 	echo 1 > /proc/sys/net/ipv4/ip_forward
-	iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE	
+
+	iptables -t filter -F
+	iptables -t nat -F
+
+	iptables -P FORWARD DROP
+
+	iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+	iptables -t filter -A FORWARD -p udp -s $NS2 -d $NS1 --dport 53 -j ACCEPT -m comment --comment "Transferencia de zona DNS - AXFR"
+	iptables -t filter -A FORWARD -p udp -s $NS1  -d $NS2 --sport 53 -j ACCEPT -m comment --comment "Transferencia de zona DNS - AXFR"
+
+	iptables -t filter -A FORWARD -p tcp -s $NS2 -d $NS1 --dport 53 -j ACCEPT -m comment --comment "Transferencia de zona DNS - AXFR"
+	iptables -t filter -A FORWARD -p tcp -s $NS1 -d $NS2 --sport 53 -j ACCEPT -m comment --comment "Transferencia de zona DNS - AXFR"
+
+	iptables -t filter -A FORWARD -p udp -s $NS1 -d $NS2 --dport 53 -j ACCEPT -m comment --comment "Notify DNS secundario"
+	iptables -t filter -A FORWARD -p udp -s $NS2  -d $NS1 --sport 53 -j ACCEPT -m comment --comment "Notify DNS secundario"
+
+	iptables -t filter -A FORWARD -p udp -s 0/0 -d 0/0 --dport 53 -j ACCEPT -m comment --comment "Resolução DNS"
+	iptables -t filter -A FORWARD -p udp -s 0/0 -d 0/0 --sport 53  -j ACCEPT -m comment --comment "Resolução DNS"
+
+	iptables -t filter -A FORWARD -p tcp -m multiport -s 0/0 -d 0/0 --dport 80,443 -j ACCEPT -m comment --comment "Baixar pacoter apt"
+	iptables -t filter -A FORWARD -p tcp -m multiport -s 0/0 -d 0/0 --sport 80,443 -j ACCEPT -m comment --comment "Baixar pacoter apt"
 }
 
 main() {
